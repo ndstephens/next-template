8d011872ae048ec608a2285627beb4b1f862660b
Nate Stephens <322714+ndstephens@users.noreply.github.com>
2024-02-10 21:38:33 -0500

    prettier config updates

--- prettier.config.js
+++ prettier.config.js
@@ -1,4 +1,6 @@
 module.exports = {
+  // 'prettier-plugin-tailwindcss' must be loaded last if you add others to this list
   plugins: ['prettier-plugin-tailwindcss'],
+  tailwindFunctions: ['clsx', 'cva', 'tw'],
   singleQuote: true,
 };
